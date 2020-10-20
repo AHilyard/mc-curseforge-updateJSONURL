9258bec5a712f9fecbfb0e66c7fc9cdfa8699248
Guylian Cox <hello@guylian.me>
2020-10-20 18:43:19 +0200

    feat: extract version from jar metadata

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const Koa = require('koa');
 const Router = require('@koa/router');
 const fetch = require('node-fetch');
 const joi = require('joi');
+const Zip = require('jszip');
+const Toml = require('@iarna/toml');
 
 const modIdSchema = joi.number().positive().integer().label('modId');
 
 const app = new Koa();
 const router = new Router();
+
+// TODO: grab changelog from curse by scraping page?
+// TODO: accept ?mcRange=[1.16,)
 
 router.get('/:modId', async ctx => {
   const modIdValidation = modIdSchema.validate(ctx.params.modId);
@@ -36,7 +41,7 @@ router.get('/:modId', async ctx => {
 
   const modCursePage = mod.websiteUrl;
 
-  const gameVersions = {};
+  // const gameVersions = {};
   const latestRecommendedReleases = {};
   const latestReleases = {};
 
@@ -55,7 +60,7 @@ router.get('/:modId', async ctx => {
 
       const fileDate = new Date(file.fileDate);
       if (!latestReleases[mcVersion] || latestReleases[mcVersion].date <= fileDate) {
-        latestReleases[mcVersion] = { version: modVersion, date: fileDate };
+        latestReleases[mcVersion] = { downloadUrl: file.downloadUrl, id: file.id, date: fileDate };
       }
 
       // int from 1 to 3
@@ -67,23 +72,31 @@ router.get('/:modId', async ctx => {
         !latestRecommendedReleases[mcVersion]
         || (latestRecommendedReleases[mcVersion].releaseType >= releaseType && latestRecommendedReleases[mcVersion].date <= fileDate)
       ) {
-        latestRecommendedReleases[mcVersion] = { version: modVersion, date: fileDate, releaseType };
+        latestRecommendedReleases[mcVersion] = { downloadUrl: file.downloadUrl, id: file.id, date: fileDate, releaseType };
       }
-
-      gameVersions[mcVersion] = gameVersions[mcVersion] || {};
-      // TODO: grab changelog from curse by scraping page?
-      gameVersions[mcVersion][modVersion] = `Check out the changelog on CurseForge - ${modCursePage}/files/${file.id}`;
     }
   }
 
   const promos = {};
+  const gameVersions = {};
+  const promises = [];
   for (const gameVersion of Object.keys(latestReleases)) {
-    promos[`${gameVersion}-latest`] = latestReleases[gameVersion].version;
+    promises.push(getVersionFromModFile(latestReleases[gameVersion].downloadUrl).then(version => {
+      promos[`${gameVersion}-latest`] = version;
+      gameVersions[gameVersion] = gameVersions[gameVersion] || {};
+      gameVersions[gameVersion][version] = `View the changelog on CurseForge: ${modCursePage}/files/${latestReleases[gameVersion].id}`;
+    }));
   }
 
   for (const gameVersion of Object.keys(latestRecommendedReleases)) {
-    promos[`${gameVersion}-recommended`] = latestRecommendedReleases[gameVersion].version;
+    promises.push(getVersionFromModFile(latestRecommendedReleases[gameVersion].downloadUrl).then(version => {
+      promos[`${gameVersion}-recommended`] = version;
+      gameVersions[gameVersion] = gameVersions[gameVersion] || {};
+      gameVersions[gameVersion][version] = `View the changelog on CurseForge: ${modCursePage}/files/${latestRecommendedReleases[gameVersion].id}`;
+    }));
   }
+
+  await Promise.all(promises);
 
   ctx.body = {
     homepage: modCursePage,
@@ -91,6 +104,180 @@ router.get('/:modId', async ctx => {
     promos,
   };
 });
+
+// TODO persist cache (pg? sqlite?)
+const cache = new Map();
+
+async function getVersionFromModFile(url) {
+  if (!cache.has(url)) {
+    cache.set(url, getVersionFromModFileUncached(url));
+  }
+
+  return cache.get(url);
+}
+
+async function getVersionFromModFileUncached(url) {
+  const modJar = await downloadModFile(url);
+  const data = await Zip.loadAsync(modJar);
+
+  if (data.files['mcmod.info']) {
+    const version = getVersionFromLegacyMcModInfo(await data.file("mcmod.info").async("string"));
+
+    if (version != null) {
+      return version;
+    }
+  }
+
+  if (data.files['META-INF/mods.toml']) {
+    const version = getVersionFromModsToml(await data.file("META-INF/mods.toml").async("string"));
+
+    if (version != null) {
+      return version;
+    }
+  }
+
+  if (data.files['META-INF/MANIFEST.MF']) {
+    const version = getVersionFromJarManifest(await data.file("META-INF/MANIFEST.MF").async("string"));
+
+    if (version != null) {
+      return version;
+    }
+  }
+
+  return null;
+}
+
+function getVersionFromJarManifest(fileContents) {
+  const manifest = parseJarManifest(fileContents);
+
+  if (!manifest || !manifest.main) {
+    return null;
+  }
+
+  return manifest.main['Implementation-Version'];
+}
+
+// https://github.com/limulus/jarfile/blob/master/src/Jar.js
+function parseJarManifest(manifest) {
+  var result = {"main": {}, "sections": {}}
+
+  var expectingSectionStart = false
+    , skip = 0
+    , currentSection = null
+
+  manifest = manifest.toString("utf8")
+  var lines = manifest.split(/(?:\r\n|\r|\n)/);
+  lines.forEach(function (line, i) {
+    var entry;
+    // this line may have already been processed, if so skip it
+    if (skip) {
+      skip--
+      return
+    }
+
+    // Watch for blank lines, they mean we're starting a new section
+    if (line === "") {
+      expectingSectionStart = true
+      return
+    }
+
+    // Extract the name and value from entry line
+    var pair = line.match(/^([a-z0-9_-]+): (.*)$/i)
+    if (!pair) {
+      _throwManifestParseError("expected a valid entry", i, line)
+    }
+    var name = pair[1], val = (pair[2] || "")
+
+    // Handle section start
+    if (expectingSectionStart && name !== "Name") {
+      _throwManifestParseError("expected section name", i, line)
+    }
+    else if (expectingSectionStart) {
+      currentSection = val
+      expectingSectionStart = false
+      return
+    }
+
+    // Add entry to the appropriate section
+    if (currentSection) {
+      if (!result["sections"][currentSection]) {
+        result["sections"][currentSection] = {}
+      }
+      entry = result["sections"][currentSection]
+    }
+    else {
+      entry = result["main"]
+    }
+    entry[name] = val
+    for (var j = i + 1; j < lines.length; j++) {
+      var byteLen = Buffer.byteLength(line, "utf8")
+      if (byteLen >= 70) {
+        line = lines[j]
+        if (line && line[0] === " ") {
+          // continuation lines must start with a space
+          entry[name] += line.substr(1)
+          skip++
+          continue
+        }
+      }
+      break
+    }
+  })
+
+  return result
+}
+
+function getVersionFromModsToml(fileContents) {
+  const manifest = Toml.parse(fileContents);
+
+  // TODO: what if there is more than one mod in the package? should compare with modId from somewhere
+
+  if (!manifest || !Array.isArray(manifest.mods)) {
+    return null;
+  }
+
+  const firstMod = manifest.mods[0];
+  if (!firstMod) {
+    return null;
+  }
+
+  const version = firstMod.version;
+
+  // starting with ${ means there was some attempt at substituting a var but it failed
+  // eg. version="${file.jarVersion}"
+  // we'll fallback to the next possible source of version
+  if (typeof version !== 'string' || version.startsWith('${')) {
+    return null;
+  }
+
+  return version;
+}
+
+function getVersionFromLegacyMcModInfo(fileContents) {
+  const mcModInfo = JSON.parse(fileContents);
+
+  if (!Array.isArray(mcModInfo)) {
+    return null;
+  }
+
+  // TODO: what if there is more than one mod in the package? should compare with modId from somewhere
+
+  if (!mcModInfo[0]) {
+    return null;
+  }
+
+  return mcModInfo[0].version;
+}
+
+async function downloadModFile(url) {
+  const result = await fetch(url);
+
+  return result.buffer();
+}
+
+function getFilePageId(modCursePage, fileId) {
+  return `${modCursePage}/files/${fileId}`;
+}
 
 async function getMod(modId) {
   const response = await fetch(`https://addons-ecs.forgesvc.net/api/v2/addon/${encodeURIComponent(modId)}`);
